b18f9a12e6e23901afafa77b005ee111c8413ea9
RifatAhmad10 <rifatahmad2003@icloud.com>
2024-04-17 14:59:37 +0100

    database change

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@
 $DBServer = '127.0.0.1'; // e.g 'localhost' or '127.0.0.1'
 $DBportNr = '3306'; //MySQL Server port nr.
 $DBUser   = 'root';  //the MySQL user name
-$DBPass   = '';  //the MySQL user password
+$DBPass   = 'password';  //the MySQL user password
 $DBName   = 'k00285774_Framework_16_Resteraunt'; //the MySQL database name
 
 
